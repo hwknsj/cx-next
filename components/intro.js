d1efdefb851b314dc69f4c9de15b0b449301329d
jtor94 <joel.torres@nike.com>
2021-10-20 05:33:01 -0700

    delete git bullshit

--- components/intro.js
+++
@@ -1,8 +0,0 @@
-const Intro = () => (
-  // <section className='flex flex-col items-center mt-16 mb-16 md:flex-row md:justify-between md:mb-12'>
-  <section className='flex flex-col items-center mt-16 mb-16'>
-    <h2 className='mt-5 text-center md:text-left'>Everything with style.</h2>
-  </section>
-)
-
-export default Intro
